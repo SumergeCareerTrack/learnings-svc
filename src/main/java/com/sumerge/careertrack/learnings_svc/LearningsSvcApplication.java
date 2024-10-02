347772fb6f99c94ea5e77922591b7ea64855d55c
Youssef Fahmy <youssef.tfs2@gmail.com>
2024-10-02 12:53:09 +0300

    First Commit

---
+++ src/main/java/com/sumerge/careertrack/learnings_svc/LearningsSvcApplication.java
@@ -0,0 +1,13 @@
+package com.sumerge.careertrack.learnings_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningsSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningsSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumerge/careertrack/learnings_svc/LearningsSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumerge.careertrack.learnings_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningsSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
