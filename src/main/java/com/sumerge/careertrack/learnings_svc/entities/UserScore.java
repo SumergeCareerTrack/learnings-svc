0b0737a32a070985f41cfff48ebe06dd187a4488
Youssef Fahmy <youssef.tfs2@gmail.com>
2024-10-08 15:43:15 +0300

    Implemented comparable interface for user scores

--- src/main/java/com/sumerge/careertrack/learnings_svc/entities/UserScore.java
+++ src/main/java/com/sumerge/careertrack/learnings_svc/entities/UserScore.java
@@ -11,11 +11,18 @@ import lombok.Data;
 @Data
 @Builder
 @AllArgsConstructor
-public class UserScore {
+public class UserScore implements Comparable<UserScore> {
 
     @Id
     private UUID userId;
 
     private Integer score;
+
+    @Override
+    public int compareTo(UserScore other) {
+        Integer myScore = score;
+        Integer otherScore = other.score;
+        return Integer.compare(myScore, otherScore);
+    }
 
 }
