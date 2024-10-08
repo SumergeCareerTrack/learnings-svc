fc44848958e9c592c960712ff1ffcad469cccfb2
Youssef Fahmy <youssef.tfs2@gmail.com>
2024-10-08 15:25:30 +0300

    Removed Todo

--- src/main/java/com/sumerge/careertrack/learnings_svc/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/sumerge/careertrack/learnings_svc/exceptions/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    // TODO we need to make returned STATUS CODES be clear CONFLICT & NOTFOUND
     @ExceptionHandler({ DoesNotExistException.class })
     public ResponseEntity<Object> handleDoesNotExistException(DoesNotExistException exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
